722790112e70ef7849347f5aa8e63d43becda0db
Alexander <aleksievisa@gmail.com>
2023-05-16 20:15:56 +0300

    feat: mobile users

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React, { PropsWithChildren } from 'react'
 import { DesktopSidebar } from './DesktopSidebar'
+import { MobileFooter } from './MobileFooter'
 
 async function Sidebar({ children }: PropsWithChildren) {
   return (
     <div className="h-full">
       <DesktopSidebar />
-      {/* <MobileFooter /> */}
+      <MobileFooter />
       <main className="lg:pl-20 h-full">
         {children}
       </main>
