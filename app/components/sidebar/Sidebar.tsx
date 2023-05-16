cfbecf182224cbdc9dd34f6ce03c630ecd6a9c30
Alexander <aleksievisa@gmail.com>
2023-05-16 20:01:13 +0300

    feat: desctop users

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,11 @@
 import React, { PropsWithChildren } from 'react'
+import { DesktopSidebar } from './DesktopSidebar'
 
 async function Sidebar({ children }: PropsWithChildren) {
   return (
     <div className="h-full">
-      {/* <DesktopSidebar currentUser={currentUser!} />
-      <MobileFooter /> */}
+      <DesktopSidebar />
+      {/* <MobileFooter /> */}
       <main className="lg:pl-20 h-full">
         {children}
       </main>
