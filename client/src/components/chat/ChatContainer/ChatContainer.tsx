40e849051f37588d6cba7980924fad5491610761
Alexander <aleksievisa@gmail.com>
2023-06-13 19:46:21 +0300

    feat: add message input

--- client/src/components/chat/ChatContainer/ChatContainer.tsx
+++ client/src/components/chat/ChatContainer/ChatContainer.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './ChatContainer.module.scss'
 
 export const ChatContainer = () => {
   return (
-    <div>ChatContainer</div>
+    <div className={styles.wrapper}>ChatContainer</div>
   )
 }
--- client/src/components/chat/MessageBar/MessageBar.tsx
+++ client/src/components/chat/MessageBar/MessageBar.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { BsEmojiSmile } from 'react-icons/bs'
+import { ImAttachment } from 'react-icons/im'
+import styles from './Message.module.scss'
+import { MdSend } from 'react-icons/md'
+import { FaMicrophone } from 'react-icons/fa'
 
 export const MessageBar = () => {
   return (
-    <div>MessageBar</div>
+    <div className={styles.wrapper}>
+      <div>
+        <BsEmojiSmile className={styles.icon} />
+      </div>
+      <div>
+        <ImAttachment className={styles.icon} />
+      </div>
+      <input
+        className={styles.message}
+        type='text'
+        placeholder='message...'
+      />
+      {/* <button className={styles.send}>
+        <FaMicrophone />
+      </button> */}
+      <button className={styles.send}>
+        <MdSend />
+      </button>
+      
+    </div>
   )
 }
