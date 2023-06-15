3b0e606df67adf6f09e068e6698321b825a1c559
Alexander <aleksievisa@gmail.com>
2023-06-15 15:21:45 +0300

    feat: add emoji picker

--- client/src/components/chat/MessageBar/MessageBar.tsx
+++ client/src/components/chat/MessageBar/MessageBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState, MouseEvent } from 'react'
 import { BsEmojiSmile } from 'react-icons/bs'
 import { ImAttachment } from 'react-icons/im'
 import styles from './Message.module.scss'
@@ -8,10 +8,35 @@ import { useStateProvider } from '@/context/StateContext'
 import axios from 'axios'
 import { ADD_MESSAGE_ROUTE } from '@/utils/ApiRoutes'
 import { REDUCER_CASES } from '@/context/constants'
+import EmojiPicker, { Theme, EmojiClickData } from 'emoji-picker-react'
 
 export const MessageBar = () => {
   const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider()
   const [message, setMessage] = useState('')
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false)
+  const emojiPickerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleOutsideClick = (event: globalThis.MouseEvent) => {
+      //@ts-ignore
+      if (event.target.id !== 'emoji-open') {
+        //@ts-ignore
+        if (emojiPickerRef.current && !emojiPickerRef.current.contains(event.target)) {
+          setShowEmojiPicker(false)
+        }
+      }
+    }
+    document.addEventListener('click', handleOutsideClick)
+    return () => document.removeEventListener('click', handleOutsideClick)
+  }, [])
+
+  const handleEmojiModal = () => {
+    setShowEmojiPicker(!showEmojiPicker)
+  }
+
+  const handleEmojiCLick = (emoji: EmojiClickData) => {
+    setMessage((prev) => (prev += emoji.emoji))
+  }
 
   const sendMessage = async () => {
     try {
@@ -40,8 +65,20 @@ export const MessageBar = () => {
 
   return (
     <div className={styles.wrapper}>
-      <div>
-        <BsEmojiSmile className={styles.icon} />
+      <div className={styles.emoji}>
+        {showEmojiPicker && 
+          (<div ref={emojiPickerRef} className={styles['emoji-container']}>
+            <EmojiPicker 
+              theme={Theme.DARK} 
+              onEmojiClick={handleEmojiCLick}
+            />
+          </div>)}
+        <BsEmojiSmile 
+          className={styles.icon}
+          id="emoji-open"
+          onClick={handleEmojiModal}
+        />
+        
       </div>
       <div>
         <ImAttachment className={styles.icon} />
